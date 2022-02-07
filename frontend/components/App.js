4f20c55b438239788c81670b1d40480194b0bb92
kylerostler <93017344+kylerostler@users.noreply.github.com>
2022-02-07 12:30:24 -0700

    started app and form

--- frontend/components/App.js
+++ frontend/components/App.js
@@ -1,11 +1,79 @@
-import React from 'react'
 
-export default class App extends React.Component {
+import React from 'react';
+import TodoList from './components/TodoList'
+import TodoForm from './components/Form'
+
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      todos: [
+        {
+          task: 'make dinner',
+          id: 1,
+          completed: false
+        },
+        {
+          task: 'finish gunpla',
+          id: 2,
+          completed: false
+        }
+      ]
+    }
+  }
+
+  handleAdd = (task) => {
+    const newTodo = { 
+      task: task,
+      id: Date.now(),
+      completed: false
+    }
+
+    this.setState({
+      ...this.state,
+      todos: [...this.state.todos, newTodo]
+    })
+  }
+
+  handleClear = () => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.filter(todo => {
+        return (todo.completed === false);
+      })
+    });
+  }
+
+
+  handleToggle = (clickedId) => {
+    this.setState({
+      ...this.state,
+      todos: this.state.todos.map(todo=> {
+        if (todo.id === clickedId) {
+          return {
+            ...todo,
+            completed: !todo.completed
+          }
+        }
+          return todo
+        
+      })
+    })
+  }
   render() {
+    const { todos } = this.state;
     return (
       <div>
-        Todo App
+        <h1>Todos</h1>
+
+        <TodoList  handleToggle={this.handleToggle} todos={todos}/>
+
+        <TodoForm handleAdd={this.handleAdd} />
+
+        <button onClick={this.handleClear}>Clear</button>
       </div>
-    )
+    );
   }
 }
+
+export default App;
--- frontend/components/Form.js
+++ frontend/components/Form.js
@@ -1,11 +1,31 @@
 import React from 'react'
 
-export default class Form extends React.Component {
-  render() {
-    return (
-      <div>
-        Form
-      </div>
-    )
-  }
+class Form extends React.Component {
+    constructor () {
+        super();
+        this.state = {
+            input: ""
+        }
+    }
+
+    handleSubmit = (evt) => {
+        evt.preventDefault();
+        this.props.handleAdd(this.state.input);
+    }
+
+    handleChange = (evt) => {
+        this.setState({
+            ...this.state,
+            input: evt.target.value
+        })
+    }
+
+    render() {
+        return (<form>
+            <input onChange={this.handleChange}/>
+            <button onClick={this.handleSubmit}>Add</button>
+          </form>)
+    }
 }
+
+export default Form;
